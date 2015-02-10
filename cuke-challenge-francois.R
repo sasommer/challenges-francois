05945bdacbd688331bc9fad01ddd0265568808f1
Francois Michonneau <francois.michonneau@gmail.com>
2015-02-10 18:08:53 -0500

    just a test

--- cuke-challenge-francois.R
+++ cuke-challenge-francois.R
@@ -2,3 +2,5 @@ download.file("http://r-bio.github.io/data/holothuriidae-specimens.csv",
               "data/holothuriidae-specimens.csv")
 download.file("http://r-bio.github.io/data/holothuriidae-nomina-valid.csv",
               "data/holothuriidae-nomina-valid.csv")
+
+## silly test
