e4685c2e988db1c3dc16d8c7cf8fe332c284b40d
Stephanie A. Matthews <steph43@hawaii.edu>
2018-01-14 09:44:47 -0800

    read data
    - added source link
    - read csv into R

--- cuke-challenge-francois.R
+++ cuke-challenge-francois.R
@@ -1,3 +1,5 @@
+## 1/12/17 
+## challenge from http://r-bio.github.io/challenges/#sea-cucumber
 ## Download data from course website
 download.file("http://r-bio.github.io/data/holothuriidae-specimens.csv",
               "data/holothuriidae-specimens.csv")
@@ -5,5 +7,8 @@ download.file("http://r-bio.github.io/data/holothuriidae-nomina-valid.csv",
               "data/holothuriidae-nomina-valid.csv")
 
 ## import data in R memory
-nom <- read.csv(file="data/holothuriidae-nomina-valid.csv",
-                stringsAsFactors=FALSE)
+hol <- read.csv(file="data/holothuriidae-specimens.csv", stringsAsFactors=FALSE)
+nom <- read.csv(file="data/holothuriidae-nomina-valid.csv", stringsAsFactors=FALSE)
+
+
+
